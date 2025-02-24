0e07e352c0d02d113baca07b7cd396428fe37f08
khj <soswoxm93@gmail.com>
2025-02-24 21:36:04 +0900

    temp

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'; 
+import { useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+ 
+  return (
+   <div className="footer">
+        footer text test
+   </div>
+  );
+};
+
+export default Footer;
