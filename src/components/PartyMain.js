8f91fcb0b9e9bde79c472211e6893ba31e5a46f5
khj <soswoxm93@gmail.com>
2025-02-27 03:20:47 +0900

    이제 숙련도도 밑에 부분 만들어야함

--- src/components/PartyMain.js
+++ src/components/PartyMain.js
@@ -3,20 +3,38 @@ import './PartyMain.css';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import Modal from './Modal';
+import SkillIndicator from './SkillIndicator.js';
 
 const PartyMain = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const containers = [
+ 
+  const dataContainers = [
     { id: 1, difficulty: "하드", title: "카멘 하드", people: "3/8", range: "1-3", tags: ["숙련", "무풀초"] },
     { id: 2, difficulty: "노말", title: "카멘 노말", people: "5/8", range: "1-3", tags: ["숙제", "엘릭서40"] },
     { id: 3, difficulty: "하드", title: "카멘 하드2", people: "2/8", range: "1-2", tags: ["반숙", "방풀초"] },
     
   ];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filteredContainers, setFilteredContainers] = useState(dataContainers);
+  const [selectedSkills, setSelectedSkills] = useState([]);
   const boxStyle = {
     border: "1px solid #333",
 
     textAlign: "center",
   };
+
+  const handleSelectTags = (skills) => {
+    setSelectedSkills(skills);
+    console.log(skills)
+  };//숙련도 가져오기 
+  const handleSearch = () => {
+    if (selectedSkills.length > 0) {
+      setFilteredContainers(dataContainers.filter(item =>
+        item.tags.some(tag => selectedSkills.includes(tag))
+      ));
+    } else {
+      setFilteredContainers(dataContainers);
+    }
+  };// 검색 버튼 눌러서 필터 적용
   return (
     <div className = "maincontainer">
       <div className = "box"> 
@@ -34,7 +52,7 @@ const PartyMain = () => {
       </div>
       <div className = "box"> 
         <div className = "leftcontainer">
-        {containers.map((item) => (
+        {dataContainers.map((item) => (
         <div className="partycontainer" key={item.id}>
           <div className="difficulty" style={boxStyle}>{item.difficulty}</div>
           <div className="title" style={boxStyle}>{item.title}</div>
@@ -51,7 +69,7 @@ const PartyMain = () => {
       </div>
       <div className = "box"> 
         <div className = "rightcontainer">
-          right
+        <SkillIndicator onSelectTags={handleSelectTags} />
 
         </div>
       </div>
