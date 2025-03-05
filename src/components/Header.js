372bdedade6491da2fbc6a5cc075403402e7f97d
khj <soswoxm93@gmail.com>
2025-03-06 02:44:00 +0900

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import './Header.css'; 
 import { useNavigate } from 'react-router-dom';
+import logo from '../image/logo2.png'
 
 const Header = ( {isLogin, setIsLogin}) => {
   const navigate = useNavigate();
@@ -8,16 +9,19 @@ const Header = ( {isLogin, setIsLogin}) => {
     navigate('/login');
   }
   const handleLogout = ()=>{
-    //나중에 로그아웃 처리
-    setIsLogin(false);
-    navigate('/');
+    setIsLogin("false");
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    window.location.href = "/";
   }
   return (
     <header className="header">
       <div className="header-left">
         <nav>
           <ul className="nav-list">
-            <li><a href="/">logo</a></li>
+            <li><a href="/">
+              <img src={logo} alt="로고" style={{ width: "100px", height: "auto", color: "#000000" }}></img>
+            </a></li>
             <li><a href="/findparty">파티찾기</a></li>
             <li><a href="/myparty">내 방가기</a></li>
             <li><a href="/mycharacter">내 캐릭터</a></li>
